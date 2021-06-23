0e76846f410150b7f612009f1b12843661e56e37
JulesP <poulain@c-koya.tech>
2021-06-23 17:22:06 +0200

    Remplacement en post

--- modules/web_listener.ts
+++ modules/web_listener.ts
@@ -1,13 +1,17 @@
-let debug = require("debug")("botcast:web")
-const express = require('express')
-const app = express()
+let debug = require('debug')('botcast:web');
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
-})
+app.post('/', (req: any, res: any) => {
+  console.log(req.body);
+
+  if (req.body.type === 1) {
+    res.json({ type: 1 });
+  }
+});
 
 app.listen(process.env.PORT || 6969, () => {
-  debug("Serveur lancé sur le port" + process.env.PORT || 6969)
-})
+  debug('Serveur lancé sur le port' + (process.env.PORT || 6969));
+});
